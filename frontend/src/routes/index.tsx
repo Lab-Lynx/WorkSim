140add6f14cc5f1342cf6ee55a7a36f00052a687
Fenet <fenetasmamaw48@gmail.com>
2026-09-24 15:39:44 +0300

    refactor: rename layouts directory to layout

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,8 +2,8 @@ import { lazy, Suspense } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import ProtectedRoute from './ProtectedRoute';
 import PublicRoute from './PublicRoute';
-import DashboardLayout from '@/components/layouts/DashboardLayout';
-import AuthLayout from '@/components/layouts/AuthLayout';
+import DashboardLayout from '@/components/layout/DashboardLayout';
+import AuthLayout from '@/components/layout/AuthLayout';
 import { ROUTES } from '@/constants';
 
 const LoginPage = lazy(() => import('@/pages/auth/LoginPage'));
